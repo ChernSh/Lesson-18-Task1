9612a30cea71f4a8d200362170073864ebc5d4dd
chernishovasasha <chern.sh2005@gmail.com>
2024-06-03 18:50:39 +0300

    add struct Computer

---
+++ Computer.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+struct Computer {
+	char name[100] = "";
+	float frequency = 0;
+	int RAM = 0;
+	bool DVDROM = 0;
+	float price = 0;
+
+	void showComputer() {
+		cout << "Name: " << name << endl;
+		cout << "Frequency: " << frequency <<" GHz" << endl;
+		cout << "RAM: " << RAM << " Gb" << endl;
+		cout << "DVDROM: " << (DVDROM ? "yes" : "no") << endl;
+		cout << "Price: " << price << " UAH" << endl;
+	}
+};
